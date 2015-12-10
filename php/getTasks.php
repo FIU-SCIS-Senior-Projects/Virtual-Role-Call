cb75e429d78ee3c3fce9e65fae7f7a67a2f5ef84
Peter Reidy <preid009@fiu.edu>
2015-12-09 22:39:54 -0500

    Deleted the folders outside of the Code folder

--- php/getTasks.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('DbHandler.php');
-
-$connection = new DBHandler();
-$res = $connection->getPinnedTasks();
-//convert the response to a json object
-die(json_encode($res));
-
--- php/getUsers.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('DbHandler.php');
-
-$connection = new DBHandler();
-$res = $connection->getUsers();
-//convert the response to a json object
-die(json_encode($res));
